59e19897d41436da42d67219b1d3a52d537f40d7
nelsonvalverdelt <nelsonvalverdelt@outlook.com>
2016-08-06 09:40:05 -0500

    Seson 4 - Web Services
    Web Services with WCF Part 1/2

---
+++ Sesion_4/DemoWebServices/WcfService/IService1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using WcfService.Models;
+
+namespace WcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "ListEmployees")]
+        List<EmployeeModel> ListEmployees();
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "FindEmployee/{id}")]
+        EmployeeModel FindEmployee(string id);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", RequestFormat = WebMessageFormat.Json, UriTemplate = "CreateEmployee")]
+        string CreateEmployee(Stream JsonStream);
+
+    }
+}
